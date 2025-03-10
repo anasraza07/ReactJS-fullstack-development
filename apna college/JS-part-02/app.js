8679bb1d7fb80fe6988c32504b9e16a09716b8e6
anasraza07 <ar470684@gmail.com>
2025-03-10 22:42:11 +0500

    apna colg js part 02 lectures 1 till 14 done

---
+++ apna college/JS-part-02/app.js
@@ -0,0 +1,169 @@
+// console.log("Hello world!")
+// console.log("anas raza")
+// let a = 10;
+// let b = 5;
+// console.log("sum is:", a + b)
+
+// let pencilPrice = 10;
+// let erasorPrice = 5;
+// let output = `The total price is ${pencilPrice + erasorPrice} rupees.`;
+// console.log(output);
+
+/*
+let a = 10;
+let b = 5;
+// // Arithmetic operators
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+
+// // Unary operators
+// console.log(a++);
+// console.log(++a);
+
+// Assignment operator
+b = a;
+console.log(b);
+*/
+
+// COMPARISON OPERATORS
+/*
+let age = 18;
+console.log(age > 18);
+console.log(age < 19);
+console.log(age >= 20);
+console.log(age <= 18);
+console.log(5 == 6) // equal to operator
+console.log(4 != 5) // not equal to operator
+console.log(5 === '5') // triple equal to operator
+console.log(5 == '5') 
+console.log(0 === '') 
+console.log(0 == '')
+console.log(0 == false)
+console.log(null == undefined)
+*/
+
+// 08. IF STATEMENT
+/*
+// console.log("before my if statement")
+// let age = 23;
+// if (age >= 18) {
+//     console.log("you can vote");
+// }
+// if(age > 20){
+//     console.log("you are in your 20s")
+// }
+// console.log("after my if statement")
+
+let firstName = "anas";
+if (firstName == "anas") {
+    console.log(`Welcome ${firstName}`);
+}
+*/
+
+// 09. PRACTICE Qs
+/*
+let color = "red";
+if(color == "red"){
+    console.log("Stop! light is red.")
+}
+if(color == "yellow"){
+    console.log("Slow down. light is yellow.")
+}
+if(color == "green"){
+    console.log("GO. light is green.")
+}
+*/
+
+// 10. ELSE IF STATEMENT
+/*
+let marks = 89;
+if (marks >= 80) {
+    console.log("A+")
+} else if (marks >= 70) {
+    console.log("A")
+} else if (marks >= 33) {
+    console.log("B")
+} else if (marks < 33) {
+    console.log("Fail")
+}
+
+let color = "red";
+if (color == "red") {
+    console.log("Stop! light is red.")
+} else if (color == "yellow") {
+    console.log("Slow down. light is yellow.")
+} else if (color == "green") {
+    console.log("GO. light is green.")
+}
+*/
+
+// 11. ELSE STATEMENT
+/*
+let age = 12;
+if (age >= 18) {
+    console.log("you can vote")
+} else {
+    console.log("you cannot vote");
+}
+
+let color = "blue";
+if (color == "red") {
+    console.log("Stop! light is red.")
+} else if (color == "yellow") {
+    console.log("Slow down. light is yellow.")
+} else if (color == "green") {
+    console.log("GO. light is green.")
+} else {
+    console.log("traffic light is broken")
+}
+*/
+
+// 12. PRACTICE Qs
+/*
+let size = 'S';
+if (size == 'XL') {
+    console.log("Price is Rs. 250")
+} else if (size == 'L') {
+    console.log("Price is Rs. 200")
+} else if (size == 'M') {
+    console.log("Price is Rs. 100")
+} else {
+    console.log("Price is Rs. 50")
+}
+*/
+
+// 13. NESTED IF-ELSE
+/*
+let marks = 99;
+if (marks >= 33) {
+    console.log("Pass");
+    if (marks >= 80) {
+        console.log("Grade: Outstanding")
+    } else {
+        console.log("Grade: A")
+    }
+} else {
+    console.log("Better luck next time!");
+}
+*/
+
+// 14. LOGICAL OPERATORS
+/*
+let marks = 34;
+// if(marks >= 33 && marks > 80){
+//     console.log("Pass");
+//     console.log("Grade: A");
+// }
+
+// if (!(marks < 33)) {
+//     console.log("pass");
+// }
+
+if ((marks >= 33 && marks > 80) || !false) {
+    console.log("pass");
+}
+*/
